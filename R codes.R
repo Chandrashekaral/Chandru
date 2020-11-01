d000e45806b448b6e5ae25df3723c4b61e3b3c82
Chandrashekaral <73770723+Chandrashekaral@users.noreply.github.com>
2020-11-01 11:53:31 +0530

    Add files via upload

---
+++ R codes.R
@@ -0,0 +1,59 @@
+1+1
+2+3*4
+3^2
+exp(1)
+X <-23
+Y <-26
+Z <-30
+X*Y*Z
+X+Y+Z
+X-Y-Z
+X<-c(2,0,0,4)
+Y<-c(1,9,9,2)
+X+Y
+X*4
+sqrt(X)
+X<-c(2,0,0,4)
+X[1]
+X[-1]
+X[-1] <- 3 ;X
+X[-1] = 5;X
+Y<9
+Y[Y<9] = 2
+
+Y[Y<9] = 2
+Y[Y<9]  = 2
+Y[Y>1]  = 2
+df < data.frame(X=1:3,Y=c("a","b","c"))
+df <-data.frame(X=c(1,2,3), Y=c("a","b","c"))
+
+
+
+
+
+
+
+df <-data.frame(x=c(1,2,3), Y=c('A','B','C'))
+
+
+
+Chandru <-data.frame(X=c(1,2,3), Y=c('A','B','C'))
+Chandru
+
+shekar <-data.frame(Name=c(Chandru,Chethan,Yusuf,Tilak),Marks=c('1','2','3','4'))
+
+df[1,1]
+df[1,c(1,2)]
+df[1,c(1,2)]
+df[1,(2),3(2)]                
+
+df[1,2]
+
+df[1,1]
+
+df[1,c(1,2)]
+
+df [c(1,3),2]
+df[c(1,3),2]
+df[c(1,3),1],c(1,3),3)]
+df[c(1:2,1:2)]
